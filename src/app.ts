66da7cf98d3e30d5b81b7c2f248a31206095ea7c
Luan Hubner <luanhubner.pro@gmail.com>
2026-04-19 12:12:14 -0400

    feat: add AuthenticateHandler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import Fastify from 'fastify'
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import fastifyJwt from '@fastify/jwt'
 import { InMemoryEventBus } from 'src/shared/event-bus'
 import { identityModule } from 'src/modules/identity'
 import { notificationModule } from 'src/modules/notification'
@@ -8,6 +9,21 @@ const app = Fastify()
 const eventBus = new InMemoryEventBus()
 
 notificationModule(eventBus)
+
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET as string,
+})
+
+app.decorate(
+  'authenticate',
+  async function (request: FastifyRequest, reply: FastifyReply) {
+    try {
+      await request.jwtVerify()
+    } catch (err) {
+      reply.send(err)
+    }
+  },
+)
 
 app.register(identityModule, { eventBus })
 
